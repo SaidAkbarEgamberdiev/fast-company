f026b19bac6c446fafe010f4a65babf45551e21b
SaidAkbarEgamberdiev <johnreedreal@gmail.com>
2021-09-21 16:16:35 +0500

    commit for pagination lesson

---
+++ src/app/components/user.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Qualitie from "./quality";
+import BookMark from "./bookmark";
+import PropTypes from "prop-types";
+const User = ({
+    _id,
+    name,
+    qualities,
+    profession,
+    completedMeetings,
+    rate,
+    onDelete,
+    bookmark,
+    onToggleBookMark
+}) => {
+    return (
+        <tr key={_id}>
+            <td>{name}</td>
+            <td>
+                {qualities.map((qual) => (
+                    <Qualitie key={qual._id} {...qual} />
+                ))}
+            </td>
+            <td>{profession.name}</td>
+            <td>{completedMeetings}</td>
+            <td>{rate} /5</td>
+            <td>
+                <BookMark
+                    status={bookmark}
+                    onClick={() => onToggleBookMark(_id)}
+                />
+            </td>
+            <td>
+                <button
+                    onClick={() => onDelete(_id)}
+                    className="btn btn-danger"
+                >
+                    delete
+                </button>
+            </td>
+        </tr>
+    );
+};
+User.propTypes = {
+    _id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    qualities: PropTypes.array.isRequired,
+    profession: PropTypes.string.isRequired,
+    completedMeetings: PropTypes.number.isRequired,
+    rate: PropTypes.number.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    bookmark: PropTypes.number.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired
+};
+export default User;
